be387429fe3ab2005e84c427b37dc9c61d77b203
vjgaur <vijayendra.gaur@gmail.com>
2023-12-08 21:52:10 +0530

    wallet descriptor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::env;
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     dotenv::from_filename(".env").ok();
+    dotenv::dotenv().ok();
     let descriptor = env::var("WALLET_DESCRIPTOR").unwrap();
+    println!("Descriptor: {}", descriptor);
+    dbg!(descriptor);
+    Ok(())
 }
