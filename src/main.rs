24ba9781fa59ad56c9e231236152ee6aa7df57f8
vjgaur <vijayendra.gaur@gmail.com>
2023-12-11 14:28:50 +0530

    creating wallet instance

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
+use bdk::{bitcoin::Network, database::MemoryDatabase, wallet, Wallet};
 use std::env;
-
 fn main() -> anyhow::Result<()> {
     dotenv::from_filename(".env").ok();
     dotenv::dotenv().ok();
     let descriptor = env::var("WALLET_DESCRIPTOR").unwrap();
     println!("Descriptor: {}", descriptor);
     dbg!(descriptor);
+    let wallet = Wallet::new(
+        descriptor,
+        None,
+        Network::Testnet,
+        MemoryDatabase::default(),
+    )?;
+
     Ok(())
 }
